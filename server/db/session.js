24b3cd1ec083651497550fb052560459dd29f761
Phùng Đức Nhật <ducnhatphung@gmail.com>
2017-05-27 18:12:15 +0700

    update: docs

--- server/db/session.js
+++ server/db/session.js
@@ -1,6 +1,12 @@
 const secret = require('./secret')
 const neo4j = require('neo4j-driver').v1;
 
-const driver = neo4j.driver('bolt://localhost', neo4j.auth.basic(secret.username, secret.password));
+const driver = neo4j.driver(
+  'bolt://localhost',
+  neo4j.auth.basic(
+    secret.username,
+    secret.password
+  )
+);
 
 module.exports = driver.session();
